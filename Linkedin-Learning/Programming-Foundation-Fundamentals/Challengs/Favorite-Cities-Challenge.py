ffe6b2b6ed26c5d908d7a2f60f92c5a51c89f5b6
abdelaziz <abdelaziz.abdelfatah98@gmail.com>
2022-04-04 15:31:30 +0200

    Uploading first course Programming-Foundation-Fundamentals

---
+++ Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Favorite-Cities-Challenge.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def favorite_city(name):
+	print("One of my favorite cities is ", name)
+
+favorite_city("Alex")
+favorite_city("Cairo")
+favorite_city("Zagazig")
+
---
+++ Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Guessing-Game-Challenge.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+x = input("What's my favorite food? ")
+if x == "Pizza" or x == "pizza":
+	print("Yep!So amazing")
+else:
+	print("Yuck! That's not it!")
+print("Thanks for playing!")
+
+
